7bb9707c69e8e252e35d1267df4d73eb969c25f0
Guillaume LEGRU <guillaume.legru@soprasteria.com>
2017-10-24 16:55:34 +0200

    New course : Spring Fundamentals

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ https://www.pluralsight.com
 
 COURSES TRACKER
 * Data Science with R (_DONE_)
-* Getting Started with Data Analysis Using Python (_IN PROGRESS_)
+* Getting Started with Data Analysis Using Python (_DONE_)
+* Spring Fundamentals (_IN PROGRESS_)
---
+++ Spring_Fundamentals/spring_sample/src/main/java/com/pluralsight/model/Customer.java
@@ -0,0 +1,27 @@
+package com.pluralsight.model;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
